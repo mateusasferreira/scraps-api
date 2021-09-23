2e9f78e895226d6391c1d7df214bab81af629f6d
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2021-09-22 21:37:20 -0300

    jest and eslint config ok

---
+++ src/__test__/first.test.ts
@@ -0,0 +1,8 @@
+describe('sum', () => {
+	it('should sum two numbers', () => {
+		const sum = 4 + 4
+
+		expect(sum).toEqual(8)
+	})
+
+})
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from 'express'
 
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({message: "Hello World"})
+	res.json({message: 'Hello World'})
 })
 
-app.listen(3333, () => console.log("servidor rodando"))
+app.listen(3333, () => console.log('servidor rodando'))
