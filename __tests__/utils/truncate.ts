98f7014f6b66bc19ec26dd320384808d45bb9fc4
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2021-11-24 20:04:25 -0300

    refac: deleted truncate test function

--- __tests__/utils/truncate.ts
+++
@@ -1,9 +0,0 @@
-import {getConnection} from 'typeorm'
-
-export async function clearDB(): Promise<void> {
-	const entities = getConnection().entityMetadatas
-	for (const entity of entities) {
-		const repository = await getConnection().getRepository(entity.name)
-		await repository.query(`DELETE FROM ${entity.tableName};`)
-	}
-}
