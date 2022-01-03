635f9f1afafd17261d07b8fa6e5291b71d0d7660
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2022-01-02 22:07:56 -0300

    feat: scrap update service

--- __tests__/unit/scrapService.test.ts
+++ __tests__/unit/scrapService.test.ts
@@ -1,6 +1,7 @@
 import ScrapService from '../../src/services/ScrapsService'
 import typeorm from 'typeorm'
 import { Scrap } from '../../src/models/Scrap'
+import { User } from '../../src/models/User'
 
 const mockedTypeorm = typeorm as jest.Mocked<typeof typeorm>
 
@@ -18,5 +19,65 @@ describe('scraps service', () => {
     await ScrapService.create(options)
     expect(mockedTypeorm.getRepository(Scrap).create).toBeCalledTimes(1)
     expect(mockedTypeorm.getRepository(Scrap).save).toBeCalledTimes(1)
+  })
+
+  it('should update scrap if caller is the creator', async () => {
+    const oldScrap = {
+      id: '1',
+      content: 'old content',
+      senderId: '1',
+      receiverId: '2',
+      date: '2022-01-03'
+    };
+    
+    const newScrap = {
+      id: '1',
+      content: 'new content',
+      senderId: '1',
+      receiverId: '2',
+      date: '2022-01-03'
+    };
+
+    const user = {
+      id: '1'
+    };
+    
+    (mockedTypeorm.getRepository(Scrap).findOne as jest.Mock)
+      .mockResolvedValueOnce(oldScrap)
+      .mockResolvedValueOnce(newScrap)
+    
+    const returnedValue = await ScrapService.update(oldScrap.id, 'content', user)
+
+    expect(mockedTypeorm.getRepository(Scrap).findOne).toHaveBeenCalledTimes(2)
+    expect(mockedTypeorm.getRepository(Scrap).update).toHaveBeenCalledTimes(1)
+    expect(returnedValue).toEqual(newScrap)
+  })
+
+  it('should not update scrap is caller is not the creator', async () => {
+    const oldScrap = {
+      id: '1',
+      content: 'old content',
+      senderId: '1',
+      receiverId: '2',
+      date: '2022-01-03'
+    };
+    
+    const newScrap = {
+      id: '1',
+      content: 'new content',
+      senderId: '1',
+      receiverId: '2',
+      date: '2022-01-03'
+    };
+    
+    const user = {
+      id: '2'
+    };
+
+    (mockedTypeorm.getRepository(Scrap).findOne as jest.Mock)
+      .mockResolvedValueOnce(oldScrap)
+      .mockResolvedValueOnce(newScrap);
+
+    expect(() => ScrapService.update(oldScrap.id, 'content', user)).rejects.toThrow();
   })
 })
--- src/services/ScrapsService.ts
+++ src/services/ScrapsService.ts
@@ -15,6 +15,22 @@ class ScrapService {
 
 		return scrap
 	}
+
+	async update(scrapId, newContent, user): Promise<Scrap>{
+		const scrapRepo = getRepository(Scrap)
+		
+		const oldScrap = await scrapRepo.findOne(scrapId)
+
+		if(oldScrap.senderId !== user.id) throw new Error('Only the scrap creator can do this operation')
+		
+		await scrapRepo.update(scrapId, {
+			content: newContent
+		})
+
+		const updatedScrap = await scrapRepo.findOne(scrapId)
+
+		return updatedScrap		
+	}
 }
 
 export default new ScrapService()
