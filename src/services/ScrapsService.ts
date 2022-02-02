054be116da074b09df2534c017b44c01b0fb4da5
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2022-02-02 20:03:06 -0300

    feat: better users scraps fetch

--- src/services/ScrapsService.ts
+++ src/services/ScrapsService.ts
@@ -1,8 +1,8 @@
 import { Scrap } from '@models/Scrap'
-import { getRepository} from 'typeorm'
+import { getRepository } from 'typeorm'
 
 class ScrapService {
-	async get(scrapId): Promise<Scrap>{
+	async get(scrapId): Promise<Scrap> {
 		const scrapRepo = getRepository(Scrap)
 
 		const scrap = await scrapRepo.findOne(scrapId)
@@ -10,59 +10,69 @@ class ScrapService {
 		return scrap
 	}
 
-	async getManyByUser(userId): Promise<Scrap[]>{
+	async getManyByUser(userId): Promise<Scrap[]> {
 		const scrapRepo = getRepository(Scrap)
 
 		const scraps = await scrapRepo
 			.createQueryBuilder('scraps')
-			.addSelect('sender.username' )
-			.where('scraps.receiver = :userId', {userId})
-			.loadRelationCountAndMap('scraps.likes', 'scraps.likes')
+			.select([
+				'scraps.id',
+				'scraps.created_at',
+				'scraps.content',
+				'sender.username',
+				'sender.id',
+				'receiver.username',
+				'receiver.id',
+			])
+			.where('scraps.receiver = :userId', { userId })
 			.leftJoin('scraps.sender', 'sender')
+			.leftJoin('scraps.receiver', 'receiver')
+			.loadRelationCountAndMap('scraps.likes', 'scraps.likes')
 			.getMany()
 
 		return scraps
 	}
 
-	
-	async create(options: Partial<Scrap>): Promise<Scrap>{
+	async create(options: Partial<Scrap>): Promise<Scrap> {
 		const scrapRepo = getRepository(Scrap)
 
 		const scrap = await scrapRepo.create({
 			content: options.content,
 			senderId: options.senderId,
-			receiverId: options.receiverId
-		},)
+			receiverId: options.receiverId,
+		})
 
 		await scrapRepo.save(scrap)
 
 		return scrap
 	}
 
-	async update(scrapId, newContent, user): Promise<Scrap>{
+	async update(scrapId, newContent, user): Promise<Scrap> {
 		const scrapRepo = getRepository(Scrap)
-		
+
 		const oldScrap = await scrapRepo.findOne(scrapId)
 
-		if(oldScrap.senderId !== user.id) throw new Error('Only the scrap creator can do this operation')
-		
+		if (oldScrap.senderId !== user.id)
+			throw new Error('Only the scrap creator can do this operation')
+
 		await scrapRepo.update(scrapId, {
-			content: newContent
+			content: newContent,
 		})
 
 		const updatedScrap = await scrapRepo.findOne(scrapId)
 
-		return updatedScrap		
+		return updatedScrap
 	}
 
-	async delete(scrapId, user): Promise<void>{
+	async delete(scrapId, user): Promise<void> {
 		const scrapRepo = getRepository(Scrap)
-		
+
 		const scrap = await scrapRepo.findOne(scrapId)
 
-		if(user.id !== scrap.senderId && user.id !== scrap.receiverId) throw new Error('Caller is not scrap creator nor receiver')
+		if (user.id !== scrap.senderId && user.id !== scrap.receiverId)
+			throw new Error('Caller is not scrap creator nor receiver')
 
-		await scrapRepo.delete(scrap.id)	
+		await scrapRepo.delete(scrap.id)
 	}
 }
 
